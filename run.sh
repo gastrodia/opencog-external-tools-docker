3e4b2180f76dc32a0cb23d732e89b91a2fd55a77
qian <qian@localhost.localdomain>
2018-03-21 16:52:53 +0800

    docker-compose

--- run.sh
+++ run.sh
@@ -1,2 +1,5 @@
-sudo docker build --rm -f ./Dockerfile -t gastrodia/opencog-external-tools:latest ./
+#sudo docker build --rm -f ./Dockerfile -t gastrodia/opencog-external-tools:latest ./
 #sudo docker run -it gastrodia/opencog-external-tools
+
+sudo docker-compose -f docker-compose.yml up --build
+#sudo docker-compose -f docker-compose.yml ups
